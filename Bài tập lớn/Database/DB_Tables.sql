ddf4144e530d8f2efe1f6b3d6632d9d9d6b4bc12
namnv72 <namnv72@wru.vn>
2019-12-09 23:51:17 +0700

    Updated DB_Tables.sql

--- Bài tập lớn/Database/DB_Tables.sql
+++ Bài tập lớn/Database/DB_Tables.sql
@@ -2,26 +2,26 @@
 /*
 Create Table Khoa (
 	Ma int Identity(1, 1),
-	TenKhoa nvarchar(30)
+	TenKhoa varchar(30) CHARSET utf8
 )
 */
 Create Table NganhHoc (
-	MaNganh char(10) Not null Primary key,
-	TenNghanh nvarchar(50),
-	ChiTiet nvarchar(max)
+	MaNganh varchar(10) Not null Primary key,
+	TenNghanh varchar(50) CHARSET utf8,
+	ChiTiet varchar(21844)
 )
 Create Table MonHoc (
-	MaMon char(10) Not null Primary key,
-	MaNganh char(10),
-	TenMon nvarchar(30),
+	MaMon varchar(10) Not null Primary key,
+	MaNganh varchar(10),
+	TenMon varchar(30) CHARSET utf8,
 	SoTinChi tinyint,
 	ThucHanh bit,
 	Foreign Key (MaNganh) References NganhHoc(MaNganh),
 )
 Create Table GiangVien (
 	MaGV int Not null Primary key Identity(1, 1),
-	HoTen nvarchar(50) Not null,
-	GioiTinh nvarchar(3)
+	HoTen varchar(50) Not null CHARSET utf8,
+	GioiTinh varchar(3) CHARSET utf8
 )
 Create Table Account_GV (
 	MaGV int Not null Primary key,
@@ -31,42 +31,48 @@ Create Table Account_GV (
 )
 Create Table Lop (
 	MaLop int Not null Primary key Identity(1, 1),
-	TenLop nvarchar(30)
+	TenLop varchar(30) CHARSET utf8
 )
 Create Table ThoiGian (
 	MaTG int Not null Primary key Identity(1, 1),
-	NamHoc char(10),
-	HocKy char(2),
+	NamHoc varchar(10),
+	HocKy varchar(2),
 	GiaiDoan char(1),
 	TG_BatDau date,
 	TG_KetThuc date,
 )
 Create Table LopHocPhan (
 	Ma int Not null Primary key,
-	MaMon char(10),
+	MaMon varchar(10),
 	MaLop int,
 	MaTG int,
-	NhomTH char(2), -- 0 là lớp lý thuyết, lớp thực hành bắt đầu từ 1
+	NhomTH varchar(2), -- 0 là lớp lý thuyết, lớp thực hành bắt đầu từ 1
 	MaGV int,
 	Foreign Key (MaLop) References Lop(MaLop),
 	Foreign Key (MaMon) References MonHoc(MaMon),
 	Foreign Key (MaTG) References ThoiGian(MaTG),
 	Foreign Key (MaGV) References GiangVien(MaGV),
 )
+create Table DiaDiem
+(
+MaDD int not null Primary key identity(1,1),
+Ten varchar(20)
+)
 Create Table LichTrinh (
 	Ma int Not null Primary key Identity(1, 1),
 	MaLHP int Not null,
-	BaiHoc nvarchar(50),
-	DiaDiem varchar(15),
+	MaDD int ,
+	BaiHoc varchar(50) CHARSET utf8, 
 	Thoigian datetime,
-	Foreign Key (MaLHP) References LopHocPhan(Ma)
+	Foreign Key (MaLHP) References LopHocPhan(Ma),
+	foreign key (MaDD) References DiaDiem(MaDD)
 )
 
 Create Table NhanVien (
 	MaNV int Not null Primary key Identity(1, 1),
-	HoTen nvarchar(50) Not null,
-	GioiTinh nvarchar(3),
-	ChucVu nvarchar(50)
+	HoTen varchar(50) Not null CHARSET utf8,
+	GioiTinh varchar(3) CHARSET utf8,
+	ChucVu varchar(50) CHARSET utf8
 )
 Create Table Account_NV (
 	MaNV int Not null Primary key,

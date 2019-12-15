3525a2cb7d523ab2a54efc845d227b23f7744f52
thanhle1547 <thanhlm72@wru.vn>
2019-12-15 11:07:33 +0700

    Update DB_Tables.sql
    Added 2 Table: Category & News, also changed CHARSET from utf8 to utf8mb4

--- Bài tập lớn/Database/DB_Tables.sql
+++ Bài tập lớn/Database/DB_Tables.sql
@@ -1,82 +1,98 @@
 ﻿Create Database DH_ThuyLoi
 /*
 Create Table Khoa (
-	Ma int Identity(1, 1),
-	TenKhoa varchar(30) CHARSET utf8
+	Ma tinyint UNSIGNED UNSIGNED AUTO_INCREMENT,
+	TenKhoa varchar(30) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci
 )
 */
 Create Table NganhHoc (
 	MaNganh varchar(10) Not null Primary key,
-	TenNghanh varchar(50) CHARSET utf8,
+	TenNghanh varchar(50) Not null CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci, -- utf8 -> utf8mb4
 	ChiTiet varchar(21844)
 )
 Create Table MonHoc (
 	MaMon varchar(10) Not null Primary key,
-	MaNganh varchar(10),
-	TenMon varchar(30) CHARSET utf8,
-	SoTinChi tinyint,
-	ThucHanh bit,
-	Foreign Key (MaNganh) References NganhHoc(MaNganh),
+	MaNganh varchar(10) Not null,
+	TenMon varchar(30) Not null CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci,
+	SoTinChi tinyint UNSIGNED,
+	ThucHanh boolean,
+	Foreign Key (MaNganh) References NganhHoc(MaNganh)
 )
 Create Table GiangVien (
-	MaGV int Not null Primary key Identity(1, 1),
-	HoTen varchar(50) Not null CHARSET utf8,
-	GioiTinh varchar(3) CHARSET utf8
+	MaGV tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	HoTen varchar(50) Not null CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci,
+	GioiTinh varchar(3) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci
 )
 Create Table Account_GV (
-	MaGV int Not null Primary key,
-	Username varchar(20),
-	Password varchar(16),
+	MaGV tinyint UNSIGNED Not null Primary key,
+	Username varchar(20) Not null,
+	Password char(64) Not null, -- using SHA-256
 	Foreign Key (MaGV) References GiangVien(MaGV)
 )
 Create Table Lop (
-	MaLop int Not null Primary key Identity(1, 1),
-	TenLop varchar(30) CHARSET utf8
+	MaLop tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	TenLop varchar(30) Not null CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci
 )
 Create Table ThoiGian (
-	MaTG int Not null Primary key Identity(1, 1),
-	NamHoc varchar(10),
-	HocKy varchar(2),
-	GiaiDoan char(1),
-	TG_BatDau date,
-	TG_KetThuc date,
+	MaTG tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	NamHoc varchar(9) Not null,
+	HocKy varchar(1) Not null,
+	GiaiDoan char(1) Not null,
+	TG_BatDau date Not null,
+	TG_KetThuc date Not null,
 )
 Create Table LopHocPhan (
-	Ma int Not null Primary key,
-	MaMon varchar(10),
-	MaLop int,
-	MaTG int,
+	MaLHP tinyint UNSIGNED Not null Primary key,
+	MaMon varchar(10) Not null
+	MaLop tinyint UNSIGNED Not null,
+	MaTG tinyint UNSIGNED Not null,
 	NhomTH varchar(2), -- 0 là lớp lý thuyết, lớp thực hành bắt đầu từ 1
-	MaGV int,
+	MaGV tinyint UNSIGNED Not null,
 	Foreign Key (MaLop) References Lop(MaLop),
 	Foreign Key (MaMon) References MonHoc(MaMon),
 	Foreign Key (MaTG) References ThoiGian(MaTG),
 	Foreign Key (MaGV) References GiangVien(MaGV),
 )
-create Table DiaDiem
+Create Table DiaDiem
 (
-MaDD int not null Primary key identity(1,1),
-Ten varchar(20)
+	MaDD tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	TenDD varchar(20) Not null
 )
 Create Table LichTrinh (
-	Ma int Not null Primary key Identity(1, 1),
-	MaLHP int Not null,
-	MaDD int ,
-	BaiHoc varchar(50) CHARSET utf8, 
+	MaLT tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	MaLHP tinyint UNSIGNED Not null,
+	MaDD tinyint UNSIGNED Not null,
+	BaiHoc varchar(50) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci, 
 	Thoigian datetime,
-	Foreign Key (MaLHP) References LopHocPhan(Ma),
+	Foreign Key (MaLHP) References LopHocPhan(MaMaLHP),
 	foreign key (MaDD) References DiaDiem(MaDD)
 )
 
+
 Create Table NhanVien (
-	MaNV int Not null Primary key Identity(1, 1),
-	HoTen varchar(50) Not null CHARSET utf8,
-	GioiTinh varchar(3) CHARSET utf8,
-	ChucVu varchar(50) CHARSET utf8
+	MaNV tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	HoTen varchar(25) Not null CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci,
+	GioiTinh varchar(3) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci,
+	ChucVu varchar(25) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci
 )
 Create Table Account_NV (
-	MaNV int Not null Primary key,
+	MaNV tinyint UNSIGNED Not null Primary key,
 	Username varchar(20),
-	Password varchar(16),
+	Password char(64),
 	Foreign Key (MaNV) References NhanVien(MaNV)
+)
+
+
+Create Table DanhMucTT (
+	MaDM tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	TenDM varchar(120) Not null CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci
+)
+Create Table TinTuc (
+	MaTin tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	TieuDe varchar(120) Not null CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci,
+	TomTat varchar(500) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci,
+	HinhAnh mediumblob Not null, --  ~ 16 megabytes
+	MoTa varchar(21844) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci,
+	MaDM tinyint UNSIGNED Not null,
+	Foreign Key (MaDM) References DanhMucTT(MaDM)
 )

9e7fa0e98e6eb5c1443a6d96493ad94656181faf
thanhle1547 <thanhlm72@wru.vn>
2019-12-28 09:50:25 +0700

    Modified html, css

--- Bài tập lớn/Front-End/js/script.js
+++ Bài tập lớn/Front-End/js/script.js
@@ -1,4 +1,4 @@
-//Get the button:
+/* //Get the button:
 var mybutton = document.getElementById("back-to-top");
 // mybutton.onclick = topFunction();
 // When the user scrolls down 20px from the top of the document, show the button
@@ -16,4 +16,34 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+} */
+
+$(document).ready(function(){
+  var app_header = $('.app-header');
+  $(window).scroll(function(e){
+    if(app_header.offset().top !== 0) {
+      if (!app_header.hasClass('shadow'))
+        app_header.addClass('shadow');
+    }
+    else
+      app_header.removeClass('shadow');
+  });
+
+  var submenu_title = $('.submenu-title');
+  submenu_title.click(function(){
+    let submenu = submenu_title.next();
+    if (submenu.hasClass('show'))
+      submenu.removeClass('show');
+    else
+      submenu.addClass('show');
+  });
+
+  var btn_menu = $('#btn-menu');
+  var nav_right = $('.nav-right');
+  btn_menu.click(function(){
+    if (!btn_menu.is(':checked'))
+      nav_right.removeClass('hide');
+    else
+      nav_right.addClass('hide');
+  });
+});

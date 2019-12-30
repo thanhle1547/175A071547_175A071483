6de479743d25788fc9f74eaab63b8f0bea1f42b7
thanhle1547 <thanhlm72@wru.vn>
2019-12-30 23:34:50 +0700

    Changed Database Tables

--- Bài tập lớn/Database/DB_Tables v2.sql
+++ Bài tập lớn/Database/DB_Tables v2.sql
@@ -1,9 +1,5 @@
-﻿/*
+/*
 Create Database DH_ThuyLoi
-Create Table Khoa (
-	Ma tinyint UNSIGNED UNSIGNED AUTO_INCREMENT,
-	TenKhoa varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
-)
 */
 Create Table NganhHoc (
 	MaNganh varchar(10) Not null Primary key,
@@ -14,19 +10,33 @@ Create Table MonHoc (
 	MaMon varchar(10) Not null Primary key,
 	MaNganh varchar(10) Not null,
 	TenMon varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci Not null,
-	SoTinChi tinyint(1) UNSIGNED,
-	SoTiet tinyint(2) UNSIGNED,
-	ThucHanh boolean,
 	Foreign Key (MaNganh) References NganhHoc(MaNganh)
 );
-Create Table GiangVien (
-	MaGV tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+
+Create Table ChucVu (
+    MaCV tinyint(10) UNSIGNED Not null,
+    ChucVu varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci Not null
+);
+Create Table NguoiDung (
+	MaND tinyint(20) UNSIGNED Not null Primary key AUTO_INCREMENT,
 	HoTen varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci Not null,
 	GioiTinh varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
 	SDT varchar(10),
+    Username varchar(20) Not null,
+	Password varchar(64) Not null, -- using SHA-256
+	Salt varchar(100) Not null,
+	ValidationCode varchar(100) Not null,
+	Active boolean DEFAULT 0,
+    MaCV tinyint(10) UNSIGNED Not null,
+	Foreign Key (MaCV) References ChucVu(MaCV)
+);
+Create Table GiangVien (
+	MaGV tinyint(10) UNSIGNED Not null Primary key,
 	MaNganh varchar(10) Not null,
+	Foreign Key (MaGV) References NguoiDung(MaND)
 	Foreign Key (MaNganh) References NganhHoc(MaNganh)
 );
+
 Create Table GV_MonHoc (
 	MaGV_MH tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
 	MaGV tinyint UNSIGNED Not null,
@@ -34,21 +44,12 @@ Create Table GV_MonHoc (
 	Foreign key (MaGV) References GiangVien(MaGV),
 	Foreign key (MaMon) References MonHoc(MaMon)
 );
-Create Table Account_GV (
-	MaGV tinyint UNSIGNED Not null Primary key,
-	Username varchar(20) Not null,
-	Password varchar(64) Not null, -- using SHA-256
-	Salt varchar(100) Not null,
-	ValidationCode varchar(100) Not null,
-	Active boolean DEFAULT 0,
-	Foreign Key (MaGV) References GiangVien(MaGV)
-);
 Create Table Lop (
 	MaLop tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
 	TenLop varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci Not null
 );
 Create Table ThoiGian (
-	MaTG tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	MaTG tinyint(10) UNSIGNED Not null Primary key AUTO_INCREMENT,
 	NamHoc varchar(9) Not null,
 	HocKy char(1) Not null,
 	GiaiDoan char(1) Not null,
@@ -56,47 +57,52 @@ Create Table ThoiGian (
 	TG_KetThuc date Not null
 );
 Create Table LopHocPhan (
-	MaLHP tinyint UNSIGNED Not null Primary key,
+	MaLHP tinyint(100) UNSIGNED Not null Primary key,
 	MaGV_MH tinyint UNSIGNED Not null,
 	MaLop tinyint UNSIGNED Not null,
-	MaTG tinyint UNSIGNED Not null,
-	NhomTH varchar(2), -- 0 là lớp lý thuyết, lớp thực hành bắt đầu từ 1
+	MaTG tinyint(10) UNSIGNED Not null,
+	NhomTH tinyint(1), -- 0 là lớp lý thuyết, lớp thực hành bắt đầu từ 1
+    SoTuan tinyint(1) Not null,
+    SoTinChi tinyint(1) UNSIGNED Not null,
+	SoTietLyThuyet tinyint(2) UNSIGNED Not null,
+	SoTietThucHanh tinyint(2) UNSIGNED DEFAULT null,
+	SoTietThaoLuan tinyint(2) UNSIGNED DEFAULT null,
 	Foreign Key (MaGV_MH) References GV_MonHoc(MaGV_MH),
 	Foreign Key (MaLop) References Lop(MaLop),
 	Foreign Key (MaTG) References ThoiGian(MaTG)
 );
+
 Create Table DiaDiem
 (
 	MaDD tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
 	TenDD varchar(20) Not null
 );
-Create Table LichTrinh (
+Create Table KeHoach (
 	MaLT tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
 	MaLHP tinyint UNSIGNED Not null,
 	MaDD tinyint UNSIGNED Not null,
-	BaiHoc varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci, 
 	Thoigian datetime,
 	Foreign Key (MaLHP) References LopHocPhan(MaLHP),
 	Foreign key (MaDD) References DiaDiem(MaDD)
 );
-
-
-
-Create Table NhanVien (
-	MaNV tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
-	HoTen varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci Not null,
-	GioiTinh varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-	ChucVu varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci Not null
+Create Table LichTrinh (
+	MaLT tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	MaLHP tinyint UNSIGNED Not null,
+	MaDD tinyint UNSIGNED Not null,
+	Thoigian datetime,
+    Nghi boolean DEFAULT 0,
+    HocBu boolean DEFAULT 0,
+	Foreign Key (MaLHP) References LopHocPhan(MaLHP),
+	Foreign key (MaDD) References DiaDiem(MaDD)
 );
-Create Table Account_NV (
-	MaNV tinyint UNSIGNED Not null Primary key,
-	Username varchar(20) Not null,
-	Password varchar(64) Not null,
-	Salt varchar(100) Not null,
-	ValidationCode varchar(100) Not null,
-	Active boolean DEFAULT 0,
-	Foreign Key (MaNV) References NhanVien(MaNV)
+Create Table ND_GiangDay (
+	MaLT tinyint UNSIGNED Not null Primary key,
+	BaiHoc varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci, 
+    SoTiet tinyint(1) UNSIGNED,
+	Foreign Key (MaLHP) References LopHocPhan(MaLHP),
+	Foreign key (MaDD) References DiaDiem(MaDD)
 );
+
 
 
 Create Table DanhMucBV (

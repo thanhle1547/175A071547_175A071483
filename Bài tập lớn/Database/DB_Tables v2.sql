88ffa70ff7e571424e2eb03cd18092f5abf6e04b
thanhle1547 <thanhlm72@wru.vn>
2020-01-06 13:06:10 +0700

    Changed Data Tables

--- Bài tập lớn/Database/DB_Tables v2.sql
+++ Bài tập lớn/Database/DB_Tables v2.sql
@@ -48,6 +48,7 @@ Create Table Lop (
 	MaLop tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
 	TenLop varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci Not null
 );
+
 Create Table ThoiGian (
 	MaTG tinyint(10) UNSIGNED Not null Primary key AUTO_INCREMENT,
 	NamHoc varchar(9) Not null,
@@ -56,51 +57,73 @@ Create Table ThoiGian (
 	TG_BatDau date Not null,
 	TG_KetThuc date Not null
 );
-Create Table LopHocPhan (
-	MaLHP tinyint(100) UNSIGNED Not null Primary key,
-	MaGV_MH tinyint UNSIGNED Not null,
-	MaLop tinyint UNSIGNED Not null,
-	MaTG tinyint(10) UNSIGNED Not null,
-	NhomTH tinyint(1), -- 0 là lớp lý thuyết, lớp thực hành bắt đầu từ 1
-    SoTuan tinyint(1) Not null,
-    SoTinChi tinyint(1) UNSIGNED Not null,
-	SoTietLyThuyet tinyint(2) UNSIGNED Not null,
-	SoTietThucHanh tinyint(2) UNSIGNED DEFAULT null,
-	SoTietThaoLuan tinyint(2) UNSIGNED DEFAULT null,
-	Foreign Key (MaGV_MH) References GV_MonHoc(MaGV_MH),
-	Foreign Key (MaLop) References Lop(MaLop),
-	Foreign Key (MaTG) References ThoiGian(MaTG)
+Create Table Tiet (
+	MaTiet tinyint(2) UNSIGNED Not null Primary key,
+	ThoiGian time Not null,
 );
-
 Create Table DiaDiem
 (
 	MaDD tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
 	TenDD varchar(20) Not null
 );
+
+Create Table LopHocPhan (
+	MaLHP tinyint(100) UNSIGNED Not null Primary key,
+	MaGV_MH tinyint UNSIGNED Not null,
+	MaLop tinyint UNSIGNED Not null,
+	MaTG tinyint(10) UNSIGNED Not null,
+-- vì lớp thực hành có thời gian lệch với lớp lý thuyết nhưng vẫn phải nằm trong thời gian được định trong bảng ThoiGian
+	TG_BatDau date DEFAULT null,
+	TG_KetThuc date DEFAULT null,
+-- lớp thực hành ko cần phải điền số tín chỉ
+    SoTinChi tinyint(1) UNSIGNED null,
+	LoaiLop ENUM('lý thuyết', 'thảo luận', 'thực hành') Not null,
+	Nhom tinyint(1) UNSIGNED Not null, -- 0 là lớp lý thuyết, lớp thực hành bắt đầu từ 1
+	Foreign Key (MaGV_MH) References GV_MonHoc(MaGV_MH),
+	Foreign Key (MaLop) References Lop(MaLop),
+	Foreign Key (MaTG) References ThoiGian(MaTG)
+);
+Create Table ChiTietLHP (
+	MaLHP tinyint(100) UNSIGNED Not null Primary key,
+	Thu tinyint(1) UNSIGNED Not null,
+-- so với các bản ghi khác có bị trùng địa điểm/tiết ko  ->> trigger insert, update
+	MaDD tinyint UNSIGNED Not null, 
+	Tiet varchar(10) Not null, -- là 1 chuỗi vd: '7,8,9', cắt ra để kiểm tra
+	Foreign key (MaDD) References DiaDiem(MaDD),
+);
+
 Create Table KeHoach (
-	MaLT tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
+	MaKH tinyint(100) UNSIGNED Not null Primary key AUTO_INCREMENT,
 	MaLHP tinyint UNSIGNED Not null,
-	MaDD tinyint UNSIGNED Not null,
-	Thoigian datetime,
+-- lớp có thời gian học kéo dài trong cả 2 giai đoạn
+    SoTuan tinyint(2) UNSIGNED Not null,
+	SoTiet tinyint(2) UNSIGNED Not null,
 	Foreign Key (MaLHP) References LopHocPhan(MaLHP),
-	Foreign key (MaDD) References DiaDiem(MaDD)
+);
+Create Table ChiTiet_KH (
+	MaKH tinyint(100) UNSIGNED Not null Primary key,
+-- so với tổng số tuần	->> trigger insert, update
+	Tuan tinyint(2) UNSIGNED Not null,
+-- kiểm tra ngày có đúng vào thứ như ChiTietLHP
+	Ngay date Not null,
+	NoiDung varchar(500), -- nd theo kế hoạch
+-- so với tổng số tiết	->> trigger insert, update
+	SoTiet tinyint(1) UNSIGNED,
+	Foreign key (MaKH) References KeHoach(MaKH)
 );
 Create Table LichTrinh (
-	MaLT tinyint UNSIGNED Not null Primary key AUTO_INCREMENT,
-	MaLHP tinyint UNSIGNED Not null,
-	MaDD tinyint UNSIGNED Not null,
-	Thoigian datetime,
-    Nghi boolean DEFAULT 0,
-    HocBu boolean DEFAULT 0,
-	Foreign Key (MaLHP) References LopHocPhan(MaLHP),
-	Foreign key (MaDD) References DiaDiem(MaDD)
-);
-Create Table ND_GiangDay (
-	MaLT tinyint UNSIGNED Not null Primary key,
-	BaiHoc varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci, 
-    SoTiet tinyint(1) UNSIGNED,
-	Foreign Key (MaLHP) References LopHocPhan(MaLHP),
-	Foreign key (MaDD) References DiaDiem(MaDD)
+	MaKH tinyint(100) UNSIGNED Not null Primary key,
+	Tuan tinyint(2) UNSIGNED DEFAULT null,
+	Ngay date DEFAULT null,
+	MaDD tinyint UNSIGNED DEFAULT null,
+	Tiet varchar(10) DEFAULT null,
+	NoiDung varchar(500), -- nd theo thực tế
+	TinhHinhLop varchar(500),
+	TrangThai ENUM('nghỉ', 'học bù') DEFAULT null,
+	MaGV tinyint(10) DEFAULT null, -- cho lóp học bù
+	Foreign Key (MaKH) References KeHoach(MaKH),
+	Foreign key (MaDD) References DiaDiem(MaDD),
+	Foreign key (MaGV) References GiangVien(MaGV),
 );
 
 

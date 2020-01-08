4f37403ab30689af9f7271f8cbd891e5ed8d3866
thanhle1547 <thanhlm72@wru.vn>
2020-01-08 17:42:56 +0700

    Added Databse name in config file

--- Bài tập lớn/Assignment/system/config/config.php
+++ Bài tập lớn/Assignment/system/config/config.php
@@ -9,7 +9,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', '');
+define('DB_NAME', 'dh_thuyloi');
 
 define('PATH_SYSTEM', 'system');
 define('PATH_SITE', 'site');

6603207e6f8cfb6318b5001c8768f205eb171506
thanhle1547 <thanhlm72@wru.vn>
2020-01-09 00:15:47 +0700

    Update Subjects Model and Controller

--- Bài tập lớn/Assignment/app/Controllers/Subjects_Controller.php
+++ Bài tập lớn/Assignment/app/Controllers/Subjects_Controller.php
@@ -1,9 +1,15 @@
 <?php
 defined('PATH_SYSTEM') || die("Bad request!");
 
+include PATH_APP . '/Entities/Subject_Entity.php';
+
 class Subjects_Controller extends Base_Controller {
     public function index() {
         $this->load_layout('subjects');
+    }
+
+    public function load_subjects($maNganh){
+        return Subjects_Model::load_subjects($maNganh);
     }
 }
 ?>
--- Bài tập lớn/Assignment/app/Models/Subjects_Model.php
+++ Bài tập lớn/Assignment/app/Models/Subjects_Model.php
@@ -1,12 +1,14 @@
 <?php
 class Subjects_Model {
-    public function load_subject($maNganh) {
+    public static function load_subjects($maNganh) {
         $query = "Select * from dh_thuyloi.MonHoc whrere MaNganh = :maNganh";
 
         if ($stmt = DB::$instance->prepare($query)){
             $stmt->bindParam(':MaNganh', $maNganh, PDO::PARAM_STR, 10);
 
             if ($stmt->execute())
+                return $stmt->fetchAll(PDO::FETCH_CLASS, 'Subject');
+            return false;
         }
     }
 }

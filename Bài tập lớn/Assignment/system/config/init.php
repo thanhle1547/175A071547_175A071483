7e77fd5b231f97cb0bd65730f5d9b876e735f895
thanhle1547 <thanhlm72@wru.vn>
2020-01-08 18:48:38 +0700

    Delete init.php
    Forgot to add this file to commit

--- Bài tập lớn/Assignment/system/config/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array (
-    'default_controller'    => 'index',
-    'default_action'        => 'index',
-    '404_controller'        => 'error',
-    '404_action'            => 'index'
-);
-?>
